082782b8f3f49086612b3cda81fb02c5bad017d5
JAEHYUK-lab <afentis508@gmail.com>
2021-01-18 17:25:31 +0900

    백준 8958번 OX 퀴즈

--- src/_1DimensionalArray/_8958.java
+++ src/_1DimensionalArray/_8958.java
@@ -19,7 +19,7 @@ public class _8958 {
 			int sum = 0;
 			
 			for(int j=0; j<arr[i].length(); j++) {
-				if(arr[i].charAt(j) == 'o')
+				if(arr[i].charAt(j) == 'O')
 					cnt++;
 				else
 					cnt=0;
