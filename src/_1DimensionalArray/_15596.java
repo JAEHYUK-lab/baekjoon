e8b8a626f6a4e7db8629594edd6c5ff5adae01a5
JAEHYUK-lab <afentis508@gmail.com>
2021-01-18 18:55:53 +0900

    백준 15596번 정수 n개의 합

---
+++ src/_1DimensionalArray/_15596.java
@@ -0,0 +1,27 @@
+package _1DimensionalArray;
+
+import java.io.*;
+
+public class _15596 {
+	public static long sum(int[] a) {
+		int sum=0;
+		
+		for(int i=0; i<a.length; i++) {
+			sum += a[i];
+		}
+		System.out.println(sum);
+		return sum;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		int[] a = new int[1000000];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0; i<=n; i++) {
+			a[i] = i;
+		}
+		
+		sum(a);
+	}
+	
+}
