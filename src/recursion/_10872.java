b5de6a3474ffd2b4794bb75b64c225b9dd91e4d0
JAEHYUK-lab <afentis508@gmail.com>
2021-01-26 21:55:06 +0900

    백준 10872번 팩토리얼

--- src/recursion/_10872.java
+++ src/recursion/_10872.java
@@ -3,14 +3,16 @@ package recursion;
 import java.io.*;
 
 public class _10872 {
+	public static int factorial(int n) {
+		if(n <= 1)
+			return 1;
+		return n*factorial(n-1);
+	}
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		double fact = 1;
-		
-		for(int i=1; i<=n; i++) {
-			fact *= i;
-		}
-		System.out.print((int)fact);
+	
+		System.out.println(factorial(n));
 	}
 }
