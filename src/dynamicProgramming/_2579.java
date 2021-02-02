e7181f72e05e7cb48a562e4d63b8de9b1497a81a
JAEHYUK-lab <afentis508@gmail.com>
2021-02-02 22:57:15 +0900

    백준 2579번 계단 오르기

--- src/dynamicProgramming/_2579.java
+++ src/dynamicProgramming/_2579.java
@@ -4,13 +4,13 @@ import java.io.*;
 
 public class _2579 {
 	public static int[] score;
-	public static Integer[] fin;
+	public static Integer[] dp;
 	
 	public static int stairs(int n) {
-		if(fin[n] == null) {
-			fin[n] = Math.max(stairs(n-2), stairs(n-3) + score[n-1]) + score[n];
+		if(dp[n] == null) {
+			dp[n] = Math.max(stairs(n-2), stairs(n-3) + score[n-1]) + score[n];
 		}
-		return fin[n];
+		return dp[n];
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -18,17 +18,17 @@ public class _2579 {
 		
 		int n = Integer.parseInt(br.readLine());
 		score = new int[n+1];
-		fin = new Integer[n+1];
+		dp = new Integer[n+1];
 		
 		for(int i=1; i<=n; i++) {
 			score[i] = Integer.parseInt(br.readLine());
 		}
 		
-		fin[0] = score[0];
-		fin[1] = score[1];
+		dp[0] = score[0];
+		dp[1] = score[1];
 		
 		if(n >= 2) {
-			fin[2] = score[1] + score[2];
+			dp[2] = score[1] + score[2];
 		}
 		
 		System.out.println(stairs(n));
